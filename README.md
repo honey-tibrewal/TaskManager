8082efb3cbda5a50ce45304ac5c03c8f847c12c0
apple <apple@192.168.0.101>
2017-03-24 20:53:01 +0530

    Final

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# TaskManager
-An Application for users to manage tasks
+**Github Link: https://github.com/honey-tibrewal/TaskManager**
+
+**Languages Used:** AngularJS,JavaScript,JQuery,HTML.
+
+**To get the screen :**
+
+- Copy the files in a folder and place them in their respective inner folders.
+- Open taskManager.Html with a browser (Chrome/Mozilla Preferred).
--- WEB-INF/js/taskManagerScript.js
+++ WEB-INF/js/taskManagerScript.js
@@ -1,14 +1,150 @@
 var taskManagementApp = angular.module('taskManagementApp',[]);
 
+taskManagementApp.directive('listEnter',function(){
+	return function(scope,element,attrs){
+		element.bind("keyup",function(event){
+			if(event.which === 13){
+				var name = this.name;
+				var enteredVal = this.value;
+				scope.$apply(function(){
+					if(enteredVal.trim().length == 0){
+						alert("Please Enter a Name");
+					}else{
+						var scope = angular.element(document.getElementById('taskManagementHtmlBody')).scope();
+						if(scope.lists.length != 0){
+							for(var i=0;i<scope.lists.length;i++){
+								if(scope.lists[i].index == name){
+									var prevName = scope.lists[i].name;
+									if(prevName.trim().length != 0){
+										var index = scope.listNamesObj.indexOf(prevName);
+										scope.listNamesObj.splice(index,1);
+									}
+									scope.lists[i].name = enteredVal;
+									scope.lists[i].listNameVal = true;
+									scope.listNamesObj.push(enteredVal);
+									scope.changeListHtml();
+								}
+							}
+						}
+					}
+				});
+			}
+		});
+	}
+});
+
+taskManagementApp.directive('cardStatus',function(){
+	return function(scope,element,attrs){
+		element.bind("keyup",function(event){
+			if(event.which === 13){
+				var cardIndex = this.name;
+				var listIndex = this.id;
+				var enteredVal = this.value;
+				scope.$apply(function(){
+					if(enteredVal.trim().length == 0){
+						alert("Please enter a status");
+					}else{
+						var scope = angular.element(document.getElementById('taskManagementHtmlBody')).scope();
+						if(scope.lists.length != 0){
+							for(var i=0;i<scope.lists.length;i++){
+								if(scope.lists[i].index == listIndex){
+									var cards = scope.lists[i].cards;
+									for(var j=0;j<scope.lists[i].cards.length;j++){
+										if(scope.lists[i].cards[j].index == cardIndex){
+											scope.lists[i].cards[j].status = enteredVal;
+											scope.lists[i].cards[j].cardStatusVal = true;
+										}
+									}
+								}
+							}
+						}
+					}
+				})
+			}
+		})
+	}
+});
+
+taskManagementApp.directive('cardUser',function(){
+	return function(scope,element,attrs){
+		element.bind("keyup",function(event){
+			if(event.which === 13){
+				var cardIndex = this.name;
+				var listIndex = this.id;
+				var enteredVal = this.value;
+				scope.$apply(function(){
+					if(enteredVal.trim().length == 0){
+						alert("Please enter a UserName");
+					}else{
+						var scope = angular.element(document.getElementById('taskManagementHtmlBody')).scope();
+						if(scope.lists.length != 0){
+							for(var i=0;i<scope.lists.length;i++){
+								if(scope.lists[i].index == listIndex){
+									var cards = scope.lists[i].cards;
+									for(var j=0;j<scope.lists[i].cards.length;j++){
+										if(scope.lists[i].cards[j].index == cardIndex){
+											scope.lists[i].cards[j].userName = enteredVal;
+											scope.lists[i].cards[j].cardUserVal = true;
+										}
+									}
+								}
+							}
+						}
+					}
+				})
+			}
+		})
+	}
+});
+
+taskManagementApp.directive('cardName',function(){
+	return function(scope,element,attrs){
+		element.bind("keyup",function(event){
+			if(event.which === 13){
+				var cardIndex = this.name;
+				var listIndex = this.id;
+				var enteredVal = this.value;
+				scope.$apply(function(){
+					if(enteredVal.trim().length == 0){
+						alert("Please enter a UserName");
+					}else{
+						var scope = angular.element(document.getElementById('taskManagementHtmlBody')).scope();
+						if(scope.lists.length != 0){
+							for(var i=0;i<scope.lists.length;i++){
+								if(scope.lists[i].index == listIndex){
+									var cards = scope.lists[i].cards;
+									for(var j=0;j<scope.lists[i].cards.length;j++){
+										if(scope.lists[i].cards[j].index == cardIndex){
+											scope.lists[i].cards[j].name = enteredVal;
+											scope.lists[i].cards[j].cardNameVal = true;
+										}
+									}
+								}
+							}
+						}
+					}
+				})
+			}
+		})
+	}
+});
+
+taskManagementApp.directive('repeatDirective',function(){
+	return function(scope,element,attrs){
+		if(scope.$last){
+			scope.changeListHtml();
+		}
+	}
+});
 taskManagementApp.controller('mainController',function($scope){
 	console.log('inside mainController');
 	$scope.lists = listObjects;
-	$scope.listNames = listName;
+	$scope.listNamesObj = listNames;
 	
 	$scope.createNewList = function(){
-		var obj = {"index":lists.length,"listNameVal":false,"name":"","cards":[]};
-		$scope.lists[lists.length] = obj;
-		listObjects[lists.length] = obj;
+		var obj = {"index":$scope.lists.length,"listNameVal":false,"name":"","cards":[]};
+		$scope.lists[$scope.lists.length] = obj;
+		$scope.changeListHtml();
 	}
 	
 	$scope.createNewCard = function (listName) {
@@ -19,7 +155,7 @@ taskManagementApp.controller('mainController',function($scope){
 					var obj = {"index" : cardObj.length,"cardStatusVal":false,"status":"",
 							"cardUserVal" : false,"userName" : "" , "cardNameVal":false,"name":""}
 					$scope.lists[count].cards[cardObj.length] = obj;
-					listObjects[count].cards[cardObj.length] = obj;
+					$scope.changeListHtml();
 				}else{
 					continue;
 				}
@@ -34,7 +170,9 @@ taskManagementApp.controller('mainController',function($scope){
 					var list = $scope.lists[count];
 					var index = $scope.lists.indexOf(list);
 					$scope.lists.splice(index,1);
-					listObjects.splice(index,1);
+					var listNamesIndex = $scope.listNamesObj.indexOf(list.name);
+					$scope.listNamesObj.splice(listNamesIndex,1);
+					$scope.changeListHtml()
 				}else{
 					continue;
 				}
@@ -42,17 +180,16 @@ taskManagementApp.controller('mainController',function($scope){
 		}
 	}
 	
-	$scope.deleteCard = function(cardName,listName){
+	$scope.deleteCard = function(cardIndex,listName){
 		if($scope.lists.length != 0){
 			for(var count=0;count<$scope.lists.length;count++){
 				if($scope.lists[count].name == listName){
 					var cards = $scope.lists[count].cards;
 					for(var j=0;j<cards.length;j++){
-						if(cards[j].name == cardName){
+						if(cards[j].index == cardIndex){
 							var card = $scope.lists[count].cards[j];
 							var index = $scope.lists[count].cards.indexOf(card);
 							$scope.lists[count].cards.splice(index,1);
-							listObjects[count].cards.splice(index,1);
 						}
 					}
 				}else{
@@ -74,7 +211,6 @@ taskManagementApp.controller('mainController',function($scope){
 							card = $scope.lists[count].cards[j];
 							var index = $scope.lists[count].cards.indexOf(card);
 							$scope.lists[count].cards.splice(index,1);
-							listObjects[count].cards.splice(index,1);
 						}
 					}
 				}
@@ -82,48 +218,97 @@ taskManagementApp.controller('mainController',function($scope){
 		}
 		
 		//Add Card to the new List
-		var parent = $(obj).closest('td');
+		var parent = $(obj.target).closest('td');
 		var newListName = parent.find('#moveListName option:selected').val();
 		if($scope.lists.length != 0){
 			for(var count=0; count<$scope.lists.length;count++){
 				if($scope.lists[count].name == newListName){
 					var cardObj = $scope.lists[count].cards;
 					$scope.lists[count].cards[cardObj.length] = card;
-					listObjects[count].cards[cardObj.length] = card;
 				}
 			}
 		}
 	}
-});
-
-$('#listNameValue').on('keyup',function(e){
-	if(e.which == 13){
-		var listIndex = this.name;
-		if(this.value.trim.length == 0){
-			alert("Please enter a Name");
-		}else{
-			var scope = angular.element(document.getElementById('taskmanagementHtmlBody')).scope();
-			if(scope.lists.length != 0){
-				for(var i=0;i<scope.lists.length;i++){
-					if(scope.lists[i].index == listIndex){
-						scope.lists[i].name == this.value;
-						scope.lists[i].listNameVal = true;
-						scope.listNames.push(this.value);
+	
+	$scope.changeListName = function(listName){
+		var nameVal = listName;
+		if($scope.lists.length != 0){
+			for(var x=0;x<$scope.lists.length;x++){
+				if($scope.lists[x].name == nameVal){
+					$scope.lists[x].listNameVal = false;
+				}
+			}
+		}
+	}
+	
+	$scope.changeCardStatus = function(listName,cardIndex){
+		if($scope.lists.length != 0){
+			for(var x=0;x<$scope.lists.length;x++){
+				if($scope.lists[x].name == listName){
+					var cardsObj = $scope.lists[x].cards;
+					for(var i=0;i<cardsObj.length;i++){
+						if($scope.lists[x].cards[i].index == cardIndex){
+							$scope.lists[x].cards[i].cardStatusVal = false;
+						}
 					}
 				}
 			}
 		}
 	}
-});
-
-$('#listName').on('click',function(e){
-	var nameVal = this.html();
-	var scope = angular.element(document.getElementById('taskmanagementHtmlBody')).scope();
-	if(scope.lists.length != 0){
-		for(var x=0;x<scope.lists.length;x++){
-			if(scope.lists[i].name == nameVal){
-				scope.lists[i].listNameVal = false;
+	
+	$scope.changeCardUser = function(listName,cardIndex){
+		if($scope.lists.length != 0){
+			for(var x=0;x<$scope.lists.length;x++){
+				if($scope.lists[x].name == listName){
+					var cardsObj = $scope.lists[x].cards;
+					for(var i=0;i<cardsObj.length;i++){
+						if($scope.lists[x].cards[i].index == cardIndex){
+							$scope.lists[x].cards[i].cardUserVal = false;
+						}
+					}
+				}
 			}
 		}
 	}
+	
+	$scope.changeCardName = function(listName,cardIndex){
+		if($scope.lists.length != 0){
+			for(var x=0;x<$scope.lists.length;x++){
+				if($scope.lists[x].name == listName){
+					var cardsObj = $scope.lists[x].cards;
+					for(var i=0;i<cardsObj.length;i++){
+						if($scope.lists[x].cards[i].index == cardIndex){
+							$scope.lists[x].cards[i].cardNameVal = false;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	$scope.changeListHtml = function(){
+		var optionsHtml = '<option value=""> - Select -</option>';
+		if($scope.listNamesObj){
+			for(var i in $scope.listNamesObj){
+				optionsHtml = optionsHtml + '<option value='+$scope.listNamesObj[i]+'>'+$scope.listNamesObj[i]+'</option>';
+			}
+		}
+		var options = document.getElementsByTagName('select');
+		for(var i=0;i<options.length;i++){
+			options[i].innerHTML = optionsHtml;
+		}
+	}
+	
 });
+
+window.onbeforeunload = function(){
+	var e = e||window.event;
+	if(e){
+		saveValues();
+	}
+}
+
+function saveValues(){
+	localStorage.setItem('listObjects',JSON.stringify(listObjects));
+	localStorage.setItem('listNames', JSON.stringify(listNames));
+}
